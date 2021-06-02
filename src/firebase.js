1be381d59dbfd22927a0d0bf6d6b2a1d99a23d23
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-06-02 14:16:15 +0530

    imported prettier and firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAFsfrNVTfuAWKQpx288_1hSXZ5TAsIYoA",
+    authDomain: "customer-data101.firebaseapp.com",
+    projectId: "customer-data101",
+    storageBucket: "customer-data101.appspot.com",
+    messagingSenderId: "782822459606",
+    appId: "1:782822459606:web:bd515073631a9ccc222177",
+    measurementId: "G-SFMG40HX0F"
+};
